af2b6f8ab2447b2582c1495228221b030bb9b1c8
hoang luan <hoangluan993@gmail.com>
2015-07-20 15:47:40 +0700

    Alarm den day

---
+++ AlarmDemo/src/com/example/alarmdemo/AlarmReceiver.java
@@ -0,0 +1,24 @@
+package com.example.alarmdemo;
+
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.text.format.DateFormat;
+import android.widget.Toast;
+
+
+
+public class AlarmReceiver extends BroadcastReceiver {
+
+	@Override
+	public void onReceive(Context context, Intent intent) {
+		// TODO Auto-generated method stub
+		  Calendar now = Calendar.getInstance();
+		  java.text.DateFormat formatter = SimpleDateFormat.getTimeInstance();
+	     Toast.makeText(context, formatter.format(now.getTime()), Toast.LENGTH_SHORT).show();
+	}
+
+}
